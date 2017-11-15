a987860db9b9d2756bc4ea5ae66e5322e508d5e1
Michael Albano <albanom@albanom-macbookpro3.roam.corp.google.com>
2017-11-14 18:04:47 -0800

    fix .11ac max mcs

--- radio-count.py
+++ radio-count.py
@@ -24,7 +24,7 @@ def df_formula():
     device_airtime = (app_airtime_20 * float(ss_devices_20)) + (app_airtime_2ss_20 * float(two_ss_devices_20)) + (app_airtime_3ss_20 * float(three_ss_devices_20)) + (app_airtime_40 * float(ss_devices_40)) + (app_airtime_2ss_40 * float(two_ss_devices_40)) + (app_airtime_3ss_40 * float(three_ss_devices_40))
     #determine # of radios and round up for # of AP's
     radios = device_airtime / 80.0
-    print "you need %.2f radios" % (radios)
+    print('you need %.2f radios' % radios)
 
 # Define default Max Mbps (MCS 8) of devices for 20 & 40MHz channel widths.
 ss_20_max = 86.7 * .6
@@ -66,55 +66,50 @@ three_ss_devices_20 = raw_input("How many concurrent 20MHz three Spatial Stream 
 if not three_ss_devices_20:
     three_ss_devices_20 = 50.0
 
-modify_max = raw_input("Would you like to modify the max MCS [default 8]? Type 'help' for more info [yes,no,help] > ")
-if modify_max == 'help':
-    print "Default max MCS is MCS 8. You can enter a new value, which changes each clients max TCP/UDP capabilities (based on MCS value). For example, if you enter 0, the new MCS values will be 0, or 7.2Mbps (4.3Mbps IP tput)."
-    modify_max = raw_input("Would you like to modify the max MCS? [yes,no] > ")
+max_factor = raw_input("What will the average MCS rate be (0-9)? [Default 8] > ")
+if max_factor:
+  # change the 'max tcp/udp' values, based on user input
+  if max_factor == '0':
+    ss_20_max /=  12.0
+    two_20_max /=  12.0
+    three_20_max /= 12.0
+  elif max_factor == '1':
+    ss_20_max /= 6.0
+    two_20_max /= 6.0
+    three_20_max /= 6.0
+  elif max_factor == '2':
+    ss_20_max /= 4.0
+    two_20_max /= 4.0
+    three_20_max /= 4.0
+  elif max_factor == '3':
+    ss_20_max /= 3.0
+    two_20_max /= 3.0
+    three_20_max /= 3.0
+  elif max_factor == '4':
+    ss_20_max /= 2.0
+    two_20_max /= 2.0
+    three_20_max /= 2.0
+  elif max_factor == '5':
+    ss_20_max /=  1.5
+    two_20_max /= 1.5
+    three_20_max /= 1.5
+  elif max_factor == '6':
+    ss_20_max /= 1.33
+    two_20_max /= 1.33
+    three_20_max /= 1.33
+  elif max_factor == '7':
+    ss_20_max /= 1.2
+    two_20_max /= 1.2
+    three_20_max /= 1.2
+  elif max_factor == '9':
+    ss_20_max *= 1.11
+    two_20_max *= 1.11
+    three_20_max *= 1.11
+  # mult. by 2.08 for 40mhz
+  ss_40_max = float(2.08 * ss_20_max)
+  two_40_max = float(2.08 * two_20_max)
+  three_40_max = float(2.08 * three_20_max)
+  df_formula()
 
-if modify_max == 'yes':
-    max_factor = raw_input("what would you like the max MCS rate to be? [0-9] > ")
-    # change the 'max tcp/udp' values, based on user input
-    if max_factor == '0':
-      ss_20_max /=  12.0
-      two_20_max /=  12.0
-      three_20_max /= 12.0
-    elif max_factor == '1':
-      ss_20_max /= 6.0
-      two_20_max /= 6.0
-      three_20_max /= 6.0
-    elif max_factor == '2':
-      ss_20_max /= 4.0
-      two_20_max /= 4.0
-      three_20_max /= 4.0
-    elif max_factor == '3':
-      ss_20_max /= 3.0
-      two_20_max /= 3.0
-      three_20_max /= 3.0
-    elif max_factor == '4':
-      ss_20_max /= 2.0
-      two_20_max /= 2.0
-      three_20_max /= 2.0
-    elif max_factor == '5':
-      ss_20_max /=  1.5
-      two_20_max /= 1.5
-      three_20_max /= 1.5
-    elif max_factor == '6':
-      ss_20_max /= 1.33
-      two_20_max /= 1.33
-      three_20_max /= 1.33
-    elif max_factor == '7':
-      ss_20_max /= 1.2
-      two_20_max /= 1.2
-      three_20_max /= 1.2
-    elif max_factor == '9':
-      ss_20_max *= 1.11
-      two_20_max *= 1.11
-      three_20_max *= 1.11
-    # mult. by 2.08 for 40mhz
-    ss_40_max = float(2.08 * ss_20_max)
-    two_40_max = float(2.08 * two_20_max)
-    three_40_max = float(2.08 * three_20_max)
-
-    df_formula()
 else:
-    df_formula()
+  df_formula()
